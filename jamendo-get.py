c6d79ae8fe75742cd1a7450bde26416e961a3e02
Floor Terra <floort@gmail.com>
2010-09-10 23:22:51 +0200

    Half baked code

--- jamendo-get.py
+++ jamendo-get.py
@@ -2,9 +2,10 @@
 
 import urllib2
 import os
-
+import gzip
 
 DB_FILE = "jamendo_db.xml.tgz"
+
 
 # Download the database if it doesn't exsists yet.
 if not os.path.isfile(DB_FILE):
@@ -14,3 +15,33 @@ if not os.path.isfile(DB_FILE):
 	f.close()
 else:
 	print "Database allready downloaded."
+
+print "Reading the database"
+xml = gzip.open(DB_FILE, "r").read()
+
+print "Downloading files"
+artist_start = 0
+artist_end = artist_start
+while artist_start > -1:
+	artist_start = xml.find("<artist>", artist_end)+8
+	artist_end = xml.find("</artist>", artist_start)
+	artist = xml[xml.find("<name>",artist_start)+6:xml.find("</name>",artist_start)].replace("/","_")
+	if not os.path.isdir("download/"+artist):
+		os.mkdir("download/"+artist)
+	album_start = artist_start
+	album_end = album_start
+	while album_start < artist_end:
+		album_start = xml.find("<album>", album_end)+7
+		album_end = xml.find("</album>", album_start)
+		album = xml[xml.find("<name>",album_start)+6:xml.find("</name>",album_start)].replace("/", "_")
+		if not os.path.isdir("download/"+artist+"/"+album):
+			os.mkdir("download/"+artist+"/"+album)
+
+
+
+
+
+
+
+
+
